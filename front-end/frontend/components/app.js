d340c5ea5467c6e1d4d896a65087d2d39e58ae69
zUnknownUser <lucasamorim78c@gmail.com>
2024-09-23 22:23:34 -0300

    Salvando progresso

---
+++ front-end/frontend/components/app.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const App = () => {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/users')
+      .then(response => {
+        setMessage(response.data);
+      })
+      .catch(error => {
+        console.error('There was an error fetching the data!', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>{message}</h1>
+    </div>
+  );
+};
+
+export default App;
